a681a0179d7900e31d52a98e1773105d39c3e441
Jecy <hstogether@outlook.com>
2018-04-17 17:25:00 +0800

    Read_Img for one 28*7 hfp array.

--- CNN/CNN/minst.c
+++ CNN/CNN/minst.c
@@ -14,54 +14,75 @@ int ReverseInt(int i)
 
 ImgArr read_Img(const char* filename) // 读入图像
 {
-	FILE  *fp=NULL;
-	fp=fopen(filename,"rb");
-	if(fp==NULL)
-		printf("open file failed\n");
-	assert(fp);
+	// FILE  *fp=NULL;
+	// fp=fopen(filename,"rb");
+	// if(fp==NULL)
+	// 	printf("open file failed\n");
+	// assert(fp);
 
-	int magic_number = 0;  
-	int number_of_images = 0;  
-	int n_rows = 0;  
-	int n_cols = 0;  
-	//从文件中读取sizeof(magic_number) 个字符到 &magic_number  
-	fread((char*)&magic_number,sizeof(magic_number),1,fp); 
-	magic_number = ReverseInt(magic_number);  
-	//获取训练或测试image的个数number_of_images 
-	fread((char*)&number_of_images,sizeof(number_of_images),1,fp);  
-	number_of_images = ReverseInt(number_of_images);    
-	//获取训练或测试图像的高度Heigh  
-	fread((char*)&n_rows,sizeof(n_rows),1,fp); 
-	n_rows = ReverseInt(n_rows);                  
-	//获取训练或测试图像的宽度Width  
-	fread((char*)&n_cols,sizeof(n_cols),1,fp); 
-	n_cols = ReverseInt(n_cols);  
-	//获取第i幅图像，保存到vec中 
-	int i,r,c;
+        FILE *imfp=NULL;
+        imfp=fopen("filename","rb");
+        if(imfp==NULL) printf("open imfile faild\n"); 
 
-	// 图像数组的初始化
+	// int magic_number = 0;  
+	// int number_of_images = 0;  
+	int n_rows = 28;  
+	int n_cols = 7;  
+	// //从文件中读取sizeof(magic_number) 个字符到 &magic_number  
+	// fread((char*)&magic_number,sizeof(magic_number),1,fp); 
+	// magic_number = ReverseInt(magic_number);  
+	// //获取训练或测试image的个数number_of_images 
+	// fread((char*)&number_of_images,sizeof(number_of_images),1,fp);  
+	// number_of_images = ReverseInt(number_of_images);    
+	number_of_images = 1;    
+	// //获取训练或测试图像的高度Heigh  
+	// fread((char*)&n_rows,sizeof(n_rows),1,fp); 
+	// n_rows = ReverseInt(n_rows);                  
+	// //获取训练或测试图像的宽度Width  
+	// fread((char*)&n_cols,sizeof(n_cols),1,fp); 
+	// n_cols = ReverseInt(n_cols);  
+	// //获取第i幅图像，保存到vec中 
+	// int i,r,c;
+
+	// // 图像数组的初始化
 	ImgArr imgarr=(ImgArr)malloc(sizeof(struct MinstImgArr));
 	imgarr->ImgNum=number_of_images;
 	imgarr->ImgPtr=(MinstImg*)malloc(number_of_images*sizeof(MinstImg));
 
-	for(i = 0; i < number_of_images; ++i)  
-	{  
-		imgarr->ImgPtr[i].r=n_rows;
-		imgarr->ImgPtr[i].c=n_cols;
-		imgarr->ImgPtr[i].ImgData=(float**)malloc(n_rows*sizeof(float*));
-		for(r = 0; r < n_rows; ++r)      
-		{
-			imgarr->ImgPtr[i].ImgData[r]=(float*)malloc(n_cols*sizeof(float));
-			for(c = 0; c < n_cols; ++c)
-			{ 
-				unsigned char temp = 0;  
-				fread((char*) &temp, sizeof(temp),1,fp); 
-				imgarr->ImgPtr[i].ImgData[r][c]=(float)temp/255.0;
-			}  
-		}    
-	}
+	// for(i = 0; i < number_of_images; ++i)  
+	// {  
+	// 	imgarr->ImgPtr[i].r=n_rows;
+	// 	imgarr->ImgPtr[i].c=n_cols;
+	// 	imgarr->ImgPtr[i].ImgData=(float**)malloc(n_rows*sizeof(float*));
+	// 	for(r = 0; r < n_rows; ++r)      
+	// 	{
+	// 		imgarr->ImgPtr[i].ImgData[r]=(float*)malloc(n_cols*sizeof(float));
+	// 		for(c = 0; c < n_cols; ++c)
+	// 		{ 
+	// 			unsigned char temp = 0;  
+	// 			fread((char*) &temp, sizeof(temp),1,fp); 
+	// 			imgarr->ImgPtr[i].ImgData[r][c]=(float)temp/255.0;
+	// 		}  
+	// 	}    
+	// }
 
-	fclose(fp);
+         for(i = 0; i < number_of_images; ++i)  
+	 {  
+	 	imgarr->ImgPtr[i].ImgData=(long**)malloc(n_rows*sizeof(long*));
+	 	for(r = 0; r < n_rows; ++r)      
+	 	{
+	 		imgarr->ImgPtr[i].ImgData[r]=(long*)malloc(n_cols*sizeof(long));
+	 		for(c = 0; c < n_cols; ++c)
+	 		{ 
+	 			long temp = 0;  
+                                fscanf(imfp, "%lX", &temp);
+	 			imgarr->ImgPtr[i].ImgData[r][c]=(long)temp;
+	 		}  
+	 	}    
+	 }
+
+	// fclose(fp);
+	fclose(imfp);
 	return imgarr;
 }
 
--- CNN/CNN/minst.h
+++ CNN/CNN/minst.h
@@ -5,7 +5,7 @@ MINST数据库是一个手写图像数据库，里面
 */
 
 typedef struct MinstImg{
-	long s;           // 图像宽 和 高 [0, 0, c, r]
+	// long s;        // 图像宽 和 高 [0, 0, c, r] default
 	long** ImgData;   // 图像数据二维动态数组
 }MinstImg;
 
